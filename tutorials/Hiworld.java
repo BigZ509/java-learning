5276e241192f2203167eb63d506e746a933447bb
Marc Alexis <marcalexis86@gmail.com>
2025-08-23 15:29:00 -0400

    Applied the baics of arrays (string and int) to create a simple gradebook

---
+++ tutorials/Hiworld.java
@@ -0,0 +1,9 @@
+// headers so mostly will name of file 
+public class HiWorld {
+public static void main(String[]args) {
+    String[] studentNames = {"Lisa", "Morgan" ,"Tyler", "Quan", "Tay" };
+    int[] studentScores = {60, 35, 75, 80,98};
+    System.out.println("Congrats " + studentNames[0] + "-" + studentScores[0]);
+
+    }
+}
